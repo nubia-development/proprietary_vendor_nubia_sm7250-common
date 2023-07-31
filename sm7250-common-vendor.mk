115b3c4251d4bcd849e0a293128675f7c3348585
Davide Garberi <dade.garberi@gmail.com>
2023-07-31 03:35:16 +0200

    sm7250-common: Rename goodix fingerprint hal
    Change-Id: Iacedc5ae97dbd086b6ac240ab3500cffe14c5f88

--- sm7250-common-vendor.mk
+++ sm7250-common-vendor.mk
@@ -760,7 +760,7 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/android.hardware.gnss@2.1-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.gnss@2.1-impl-qti.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/camera.qcom.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.qcom.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/com.qti.chi.override.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/com.qti.chi.override.so \
-    vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/fingerprint.lito.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.lito.so \
+    vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/fingerprint.goodix_fod.lito.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.goodix_fod.lito.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/sound_trigger.primary.lito.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sound_trigger.primary.lito.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/vendor.qti.gnss@4.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/vendor.qti.gnss@4.0-impl.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib/hw/vendor.qti.hardware.alarm@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/vendor.qti.hardware.alarm@1.0-impl.so \
@@ -1342,7 +1342,7 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/android.hardware.gnss@2.1-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.gnss@2.1-impl-qti.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/camera.qcom.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/camera.qcom.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/com.qti.chi.override.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/com.qti.chi.override.so \
-    vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/fingerprint.lito.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.lito.so \
+    vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/fingerprint.goodix_fod.lito.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.goodix_fod.lito.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/sound_trigger.primary.lito.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/sound_trigger.primary.lito.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/vendor.qti.esepowermanager@1.1-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.esepowermanager@1.1-impl.so \
     vendor/nubia/sm7250-common/proprietary/vendor/lib64/hw/vendor.qti.gnss@4.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.gnss@4.0-impl.so \
