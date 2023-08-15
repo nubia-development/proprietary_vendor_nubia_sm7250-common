d74d3f0f837e7c882d95328444a5bce5422f99cb
Cyborg2017 <wangyang0987654@gmail.com>
2023-08-15 23:08:02 +0800

    sm7250-common: Import multicolor led firmware from nubia.20220228.184335
    Change-Id: Ic8eb0f60b04ddd0cabc2f46810c243494a796693

--- sm7250-common-vendor.mk
+++ sm7250-common-vendor.mk
@@ -580,6 +580,61 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/sm7250-common/proprietary/vendor/firmware/a650_zap.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/a650_zap.b02 \
     vendor/nubia/sm7250-common/proprietary/vendor/firmware/a650_zap.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/a650_zap.elf \
     vendor/nubia/sm7250-common/proprietary/vendor/firmware/a650_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/a650_zap.mdt \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw22xxx_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw22xxx_fw.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a0.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a1.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a2.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a3.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a3.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a4.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a4.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a5.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a5.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a6.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a6.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a7.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a7.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_a8.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_a8.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b0.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b1.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b2.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b3.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b3.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b4.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b4.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b5.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b5.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b6.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b6.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b7.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b7.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_b8.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_b8.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c0.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c1.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c2.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c3.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c3.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c4.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c4.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c5.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c5.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c6.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c6.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c7.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c7.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_c8.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_c8.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d0.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d1.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d2.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d3.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d3.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d4.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d4.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d5.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d5.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d6.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d6.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d7.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d7.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_d8.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_d8.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e0.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e1.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e2.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e3.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e3.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e4.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e4.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e5.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e5.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e6.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e6.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e7.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e7.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_e8.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_e8.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f0.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f1.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f2.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f3.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f3.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f4.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f4.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f5.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f5.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f6.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f6.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f7.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f7.bin \
+    vendor/nubia/sm7250-common/proprietary/vendor/firmware/aw_cfg_f8.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw_cfg_f8.bin \
     vendor/nubia/sm7250-common/proprietary/vendor/firmware/factory_test.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/factory_test.bin \
     vendor/nubia/sm7250-common/proprietary/vendor/firmware/ipa_fws.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b00 \
     vendor/nubia/sm7250-common/proprietary/vendor/firmware/ipa_fws.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b01 \
